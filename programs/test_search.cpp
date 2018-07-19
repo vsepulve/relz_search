cbff662a48145acdd3ec2b9c2b622f503f1de2bc
vsepulve <vsepulve@dcc.uchile.cl>
2018-07-19 16:44:19 -0300

    FactorsIteratorComparator included from its own header.

--- programs/test_search.cpp
+++ programs/test_search.cpp
@@ -22,6 +22,7 @@
 #include "TextFilterFull.h"
 
 #include "FactorsIterator.h"
+#include "FactorsIteratorComparator.h"
 
 using namespace sdsl;
 using namespace std;
@@ -74,136 +75,6 @@ void recursive_rmq_v2(unsigned int ini, unsigned int fin, unsigned int crit, rmq
 		recursive_rmq_v2(pos_max+1, fin, crit, rmq, select1_s, select1_b, select0_b, perm);
 	}
 }
-
-class FactorsIteratorComparator : public std::binary_function<unsigned int, unsigned int, bool> {
-private:
-	unsigned int n_factors;
-	rrr_vector<127>::select_1_type *select1_s;
-	rrr_vector<127>::select_1_type *select1_b;
-	rrr_vector<127>::select_0_type *select0_b;
-	inv_perm_support<> *perm;
-	inv_perm_support<> *perm_inv;
-	const char *ref_text;
-	unsigned int full_size;
-
-public:
-	FactorsIteratorComparator(){
-		n_factors = 0;
-		select1_s = NULL;
-		select1_b = NULL;
-		select0_b = NULL;
-		perm = NULL;
-		perm_inv = NULL;
-		ref_text = NULL;
-		full_size = 0;
-	}
-	
-	FactorsIteratorComparator(unsigned int _n_factors, 
-			rrr_vector<127>::select_1_type *_select1_s, 
-			rrr_vector<127>::select_1_type *_select1_b, 
-			rrr_vector<127>::select_0_type *_select0_b, 
-			inv_perm_support<> *_perm, 
-			inv_perm_support<> *_perm_inv, 
-			const char *_ref_text, 
-			unsigned int _full_size ) {
-		n_factors = _n_factors;
-		select1_s = _select1_s;
-		select1_b = _select1_b;
-		select0_b = _select0_b;
-		perm = _perm;
-		perm_inv = _perm_inv;
-		ref_text = _ref_text;
-		full_size = _full_size;
-	}
-	
-	inline bool operator()(const unsigned int a, const unsigned int b){
-		FactorsIterator it_a( a, n_factors, select1_s, select1_b, select0_b, perm, perm_inv, ref_text, full_size );
-		FactorsIterator it_b( b, n_factors, select1_s, select1_b, select0_b, perm, perm_inv, ref_text, full_size );
-		char ch_a, ch_b;
-		while( true ){
-			if( ! it_a.hasNext() ){
-				return true;
-			}
-			if( ! it_b.hasNext() ){
-				return false;
-			}
-			ch_a = it_a.next();
-			ch_b = it_b.next();
-			if( ch_a < ch_b ){
-				return true;
-			}
-			if( ch_b < ch_a){
-				return false;
-			}
-		}
-		return true;
-	}
-};
-
-class FactorsIteratorReverseComparator : public std::binary_function<unsigned int, unsigned int, bool> {
-private:
-	unsigned int n_factors;
-	rrr_vector<127>::select_1_type *select1_s;
-	rrr_vector<127>::select_1_type *select1_b;
-	rrr_vector<127>::select_0_type *select0_b;
-	inv_perm_support<> *perm;
-	inv_perm_support<> *perm_inv;
-	const char *ref_text;
-	unsigned int full_size;
-
-public:
-	FactorsIteratorReverseComparator(){
-		n_factors = 0;
-		select1_s = NULL;
-		select1_b = NULL;
-		select0_b = NULL;
-		perm = NULL;
-		perm_inv = NULL;
-		ref_text = NULL;
-		full_size = 0;
-	}
-	
-	FactorsIteratorReverseComparator(unsigned int _n_factors, 
-			rrr_vector<127>::select_1_type *_select1_s, 
-			rrr_vector<127>::select_1_type *_select1_b, 
-			rrr_vector<127>::select_0_type *_select0_b, 
-			inv_perm_support<> *_perm, 
-			inv_perm_support<> *_perm_inv, 
-			const char *_ref_text, 
-			unsigned int _full_size ) {
-		n_factors = _n_factors;
-		select1_s = _select1_s;
-		select1_b = _select1_b;
-		select0_b = _select0_b;
-		perm = _perm;
-		perm_inv = _perm_inv;
-		ref_text = _ref_text;
-		full_size = _full_size;
-	}
-	
-	inline bool operator()(const unsigned int a, const unsigned int b){
-		FactorsIteratorReverse it_a( a - 1, n_factors, select1_s, select1_b, select0_b, perm, perm_inv, ref_text, full_size );
-		FactorsIteratorReverse it_b( b - 1, n_factors, select1_s, select1_b, select0_b, perm, perm_inv, ref_text, full_size );
-		char ch_a, ch_b;
-		while( true ){
-			if( ! it_a.hasNext() ){
-				return true;
-			}
-			if( ! it_b.hasNext() ){
-				return false;
-			}
-			ch_a = it_a.next();
-			ch_b = it_b.next();
-			if( ch_a < ch_b ){
-				return true;
-			}
-			if( ch_b < ch_a){
-				return false;
-			}
-		}
-		return true;
-	}
-};
 
 unordered_map<unsigned int, FactorsIterator> mapa_iterators;
 
