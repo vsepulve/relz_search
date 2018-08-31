05dc3e4bef29d56281e7681910d86d7c8efd6b71
vsepulve <vsepulve@dcc.uchile.cl>
2018-08-31 15:29:21 -0300

    Object to define the global configuration of the data structures (and possibly add some utility functions later) added.

---
+++ include/FactorsConfig.h
@@ -0,0 +1,45 @@
+#ifndef _FACTORS_CONFIG_H
+#define _FACTORS_CONFIG_H
+
+#include <set>
+#include <map>
+#include <list>
+#include <vector>
+#include <unordered_map>
+
+#include <sdsl/suffix_arrays.hpp>
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/rmq_support.hpp>
+#include <sdsl/inv_perm_support.hpp>
+#include <sdsl/wavelet_trees.hpp>
+
+using namespace sdsl;
+using namespace std;
+
+// Type for bitarray S of the relz index, of size (reference_text + Z)
+//typedef rrr_vector<127> bits_s_type;
+typedef bit_vector bits_s_type;
+
+// Type for bitarray B of the relz index, of size (collection_text)
+//typedef rrr_vector<127> bits_b_type;
+typedef sd_vector<> bits_b_type;
+
+// Type for the fm-index
+// Default Values: wt_huff<>, 32 (Sample density for suffix array), 64 (Sample density for inverse suffix array)
+//typedef csa_wt<wt_huff<>, 32, 64, sa_order_sa_sampling<>, isa_sampling<>> fm_index_type;
+typedef csa_wt<wt_huff<>, 16, 32, sa_order_sa_sampling<>, isa_sampling<>> fm_index_type;
+
+// Type for the rmq structure
+typedef rmq_succinct_sct<false, bp_support_sada<256,32,rank_support_v5<> > > rmq_type;
+//typedef rmq_succinct_sct<false, bp_support_sada<128,32,rank_support_v5<> > > rmq_type;
+
+
+
+
+
+#endif //_FACTORS_CONFIG_H
+
+
+
+
+
---
+++ src/FactorsConfig.cpp
@@ -0,0 +1,13 @@
+#include "FactorsConfig.h"
+
+
+
+
+
+
+
+
+
+
+
+
