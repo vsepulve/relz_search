aacd088453fd66dc186e51b952e73860f4debd6c
vsepulve <vsepulve@dcc.uchile.cl>
2019-06-24 15:23:57 -0400

    Debug messages for Y tree disabled while finishing tests for X tree.

--- src/RelzIndexHash.cpp
+++ src/RelzIndexHash.cpp
@@ -128,15 +128,15 @@ RelzIndexHash::RelzIndexHash(vector<pair<unsigned int, unsigned int> > &factors,
 		arr_x[i] = arr_x_original[i];
 	}
 	
-//	for( unsigned int i = 0; i < n_factors; ++i ){
-//		cout << " arr_x[" << i << "]: " << arr_x[i] << " -> ";
-//		char c = 0;
-//		FactorsIteratorReverse it(arr_x[i] - 1, n_factors, &select1_s, &select1_b, &select0_b, &pi_inv, _ref_text, &fm_index, len_text);
-//		for(unsigned int k = 0; k < 20 && it.hasNext() && (c = it.next()) != 0; ++k ) 
-//			cout << c;
-//		cout << " (" << it.length() << ")\n";
-//	}
-//	cout << "-----\n";
+	for( unsigned int i = 0; i < n_factors; ++i ){
+		cout << " arr_x[" << i << "]: " << arr_x[i] << " -> ";
+		char c = 0;
+		FactorsIteratorReverse it(arr_x[i] - 1, n_factors, &select1_s, &select1_b, &select0_b, &pi_inv, _ref_text, &fm_index, len_text);
+		for(unsigned int k = 0; k < 20 && it.hasNext() && (c = it.next()) != 0; ++k ) 
+			cout << c;
+		cout << " (" << it.length() << ")\n";
+	}
+	cout << "-----\n";
 	
 	cout << "RelzIndexHash - Preparing arr Y\n";
 	vector<unsigned int> arr_y_original(n_factors);
@@ -267,7 +267,7 @@ RelzIndexHash::RelzIndexHash(vector<pair<unsigned int, unsigned int> > &factors,
 	timer.reset();
 	tree_y.build(full_text, len_text, factors_start, &arr_y, karp_rabin, kr_factors, false);
 	cout << "RelzIndexHash - Tree Y finished in (" << timer.getMilisec() << " ms)\n";
-	tree_y.print();
+//	tree_y.print();
 	
 	cout << "RelzIndexHash - Trees prepared in " << timer.getMilisec() << "\n";
 	timer.reset();
