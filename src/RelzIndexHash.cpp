f488373ff331680c85666cb2d6790d5641c6630d
vsepulve <vsepulve@dcc.uchile.cl>
2019-06-26 17:49:41 -0400

    Test of table search reverse disabled while testing tree Y.

--- src/RelzIndexHash.cpp
+++ src/RelzIndexHash.cpp
@@ -128,15 +128,15 @@ RelzIndexHash::RelzIndexHash(vector<pair<unsigned int, unsigned int> > &factors,
 		arr_x[i] = arr_x_original[i];
 	}
 	
-	for( unsigned int i = 0; i < n_factors; ++i ){
-		cout << " arr_x[" << i << "]: " << arr_x[i] << " -> ";
-		char c = 0;
-		FactorsIteratorReverse it(arr_x[i] - 1, n_factors, &select1_s, &select1_b, &select0_b, &pi_inv, _ref_text, &fm_index, len_text);
-		for(unsigned int k = 0; k < 20 && it.hasNext() && (c = it.next()) != 0; ++k ) 
-			cout << c;
-		cout << " (" << it.length() << ")\n";
-	}
-	cout << "-----\n";
+//	for( unsigned int i = 0; i < n_factors; ++i ){
+//		cout << " arr_x[" << i << "]: " << arr_x[i] << " -> ";
+//		char c = 0;
+//		FactorsIteratorReverse it(arr_x[i] - 1, n_factors, &select1_s, &select1_b, &select0_b, &pi_inv, _ref_text, &fm_index, len_text);
+//		for(unsigned int k = 0; k < 20 && it.hasNext() && (c = it.next()) != 0; ++k ) 
+//			cout << c;
+//		cout << " (" << it.length() << ")\n";
+//	}
+//	cout << "-----\n";
 	
 	cout << "RelzIndexHash - Preparing arr Y\n";
 	vector<unsigned int> arr_y_original(n_factors);
@@ -154,15 +154,15 @@ RelzIndexHash::RelzIndexHash(vector<pair<unsigned int, unsigned int> > &factors,
 		arr_y_inv[ arr_y_original[i] ] = i;
 	}
 	
-//	for( unsigned int i = 0; i < n_factors; ++i ){
-//		cout << " arr_y[" << i << "]: " << arr_y[i] << " -> ";
-//		char c = 0;
-//		FactorsIterator it(arr_y[i], n_factors, &select1_s, &select1_b, &select0_b, &pi_inv, _ref_text, &fm_index, len_text);
-//		for(unsigned int k = 0; k < 20 && it.hasNext() && (c = it.next()) != 0; ++k ) 
-//			cout << c;
-//		cout << " (" << it.length() << ")\n";
-//	}
-//	cout << "-----\n";
+	for( unsigned int i = 0; i < n_factors; ++i ){
+		cout << " arr_y[" << i << "]: " << arr_y[i] << " -> ";
+		char c = 0;
+		FactorsIterator it(arr_y[i], n_factors, &select1_s, &select1_b, &select0_b, &pi_inv, _ref_text, &fm_index, len_text);
+		for(unsigned int k = 0; k < 20 && it.hasNext() && (c = it.next()) != 0; ++k ) 
+			cout << c;
+		cout << " (" << it.length() << ")\n";
+	}
+	cout << "-----\n";
 
 	cout << "RelzIndexHash - X & Y prepared in " << timer.getMilisec() << "\n";
 	timer.reset();
@@ -261,13 +261,13 @@ RelzIndexHash::RelzIndexHash(vector<pair<unsigned int, unsigned int> > &factors,
 	timer.reset();
 	tree_x.build(full_text, len_text, factors_start, &arr_x, karp_rabin, kr_factors, true);
 	cout << "RelzIndexHash - Tree X finished in (" << timer.getMilisec() << " ms)\n";
-	tree_x.print();
+//	tree_x.print();
 	
 	cout << "RelzIndexHash - Building Tree Y\n";
 	timer.reset();
 	tree_y.build(full_text, len_text, factors_start, &arr_y, karp_rabin, kr_factors, false);
 	cout << "RelzIndexHash - Tree Y finished in (" << timer.getMilisec() << " ms)\n";
-//	tree_y.print();
+	tree_y.print();
 	
 	cout << "RelzIndexHash - Trees prepared in " << timer.getMilisec() << "\n";
 	timer.reset();
@@ -395,6 +395,7 @@ void RelzIndexHash::findTimes(const string &pattern, vector<unsigned int> &resul
 		timer.reset();
 		cout << "-----\n";
 		
+		/*
 		cout << "-----  tree_x.getRangeTable -----\n";
 		pair<unsigned int, unsigned int> r1_v2 = tree_x.getRangeTableRev(kr_pat_rev_vector, i, pattern_rev);
 //		querytime_p3x += timer.getNanosec();
@@ -406,6 +407,7 @@ void RelzIndexHash::findTimes(const string &pattern, vector<unsigned int> &resul
 			cout << "-----\n";
 			exit(0);
 		}
+		*/
 		
 		if( r1.first == (unsigned int)(-1) || r1.second == (unsigned int)(-1) || r1.second < r1.first ){
 			continue;
